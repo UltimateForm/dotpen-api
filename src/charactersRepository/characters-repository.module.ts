f035a58d8f9cecf2c995c99048864c3445011633
ultimateform <heliojmsemedo@gmail.com>
2024-06-08 18:13:42 +0100

    add missing commit

--- src/charactersRepository/characters-repository.module.ts
+++ src/charactersRepository/characters-repository.module.ts
@@ -1,27 +1,10 @@
 import { Module } from "@nestjs/common";
 import { CharactersRepositoryService } from "./characters-repository.service";
-import neo4j from "neo4j-driver";
-import { ConfigService } from "@nestjs/config";
+import { Neo4jModule } from "@dotpen/neo4j";
 
 @Module({
-  providers: [
-    {
-      provide: neo4j.Driver,
-      useFactory: (config: ConfigService) => {
-        const userName = config.get("NEO4J_USERNAME");
-        const password = config.get("NEO4J_PASSWORD");
-        const uri = config.get("NEO4J_URI");
-        const driverServ = neo4j.driver(
-          uri,
-          neo4j.auth.basic(userName, password),
-          { disableLosslessIntegers: true },
-        );
-        return driverServ;
-      },
-      inject: [ConfigService],
-    },
-    CharactersRepositoryService,
-  ],
+  imports: [Neo4jModule],
+  providers: [CharactersRepositoryService],
   exports: [CharactersRepositoryService],
 })
 export class CharactersRepositoryModule {}
