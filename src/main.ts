1953e26c38a2f4f1c50537acad7ea4c1c623ec41
ultimateform <heliojmsemedo@gmail.com>
2024-08-23 23:26:22 +0100

    add swagger so ECS stops complaining about health check

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { Logger } from "nestjs-pino";
 import { registerEnumType } from "@nestjs/graphql";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import {
   CharacterRelationLevel,
   CharacterRelationType,
@@ -10,6 +11,15 @@ import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  const config = new DocumentBuilder()
+    .setTitle("DotPen API")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("/", app, document, {
+    jsonDocumentUrl: "swagger.json",
+  });
   app.useGlobalPipes(new ValidationPipe());
   registerEnumType(CharacterRelationLevel, {
     name: "CharacterRelationLevel",
